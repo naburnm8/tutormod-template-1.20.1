56bee0a8542af6e68c40ab5f9bb22508b074fd8b
naburnm8 <alint@bk.ru>
2023-12-22 14:07:01 +0300

    chipi chipi chapa chapa dubi dubi daba daba

--- src/main/java/net/naburnm8/tutormod/TutorMod.java
+++ src/main/java/net/naburnm8/tutormod/TutorMod.java
@@ -11,6 +11,6 @@ public class TutorMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.info("Hello Fabric world! Faggot");
 	}
 }
